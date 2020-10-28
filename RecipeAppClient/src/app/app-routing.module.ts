4d9616c5804aced091235f156d30a7b624ccd3d6
Mushky Listvand <mushmush722@gmail.com>
2020-10-28 09:32:16 +0300

    c

--- RecipeAppClient/src/app/app-routing.module.ts
+++ RecipeAppClient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddRecipeComponent } from './Components/add-recipe/add-recipe.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'addRecipe', component: AddRecipeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- RecipeAppClient/src/app/app.module.ts
+++ RecipeAppClient/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRecipeComponent } from './Components/add-recipe/add-recipe.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecipeComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
